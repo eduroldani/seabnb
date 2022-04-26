482ba4c47985493621af9353aad71357ae7a34cd
eduardo roldan <eduroldanisaac@gmail.com>
2022-04-26 21:02:05 +0200

    User seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating Users"
+
+User.create(first_name: "Eduardo", last_name: "Roldan", email: "edu@gmail.com")
+
+puts "Seed File Done"
