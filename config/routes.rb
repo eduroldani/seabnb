03beab9532bf60caaa8c75950beed6a375234ed2
Vanya Kojouharova <vanya.kojouharova@gmail.com>
2022-04-29 21:19:54 +0200

    boats index page - boat cards and map & made home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'pages#home'
+  root to: 'boats#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 
   resources :boats do
     resources :bookings, only: [:new, :create]
   end
-
 
   resources :booking, only: [:index, :show]
 end
